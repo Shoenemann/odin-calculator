0ab15919bb9300e90ef4f008861de50fc585f064
Shoenemann <85433055+Shoenemann@users.noreply.github.com>
2022-03-14 08:24:02 +0100

    Implement statusDisplay() in js

--- script.js
+++ script.js
@@ -76,23 +76,75 @@ function clear() {
     updateDisplay()
 }
 
-function operateDisplay () {
-    if (leftNumber === NaN) return 
-    if (onScreenOperator === '') return
-    if (typeof rightNumber !== 'number') return
-    
-    const result = operate (onScreenOperator,leftNumber,rightNumber)
-    if (result === "ERRORDIVIDE0") {
-        leftNumber = NaN
-        onScreenOperator = ''
-        rightNumber = 'oh no....!'
-        updateDisplay()
-    }
+function statusDisplay () {
+    let a = leftNumber
+    let b = rightNumber
+    let op = onScreenOperator
+    if (a === NaN && op === '' && b === '') {
+        return 'empty'
+    } 
+    else if (a === NaN && op === '' && typeof b === 'number') {
+        return 'begin'
+    } 
+    else if(typeof a === 'number' && op === '' && b === '') {
+        return 'result'
+    } 
+    else if (typeof a === 'number' && op !== '' && b === '') {
+        return 'incomplete'
+    } 
+    else if(typeof a === 'number' && op !== '' && typeof b === 'number') {
+        return 'complete'
+    } 
+    else if(b !== '' && typeof b === 'string') {
+        return 'message'
+    } 
     else {
-        leftNumber = result
-        onScreenOperator = ''
-        rightNumber = ''
-        updateDisplay()
-    }    
+        return 'STATUS_ERROR'
+    }
 }
 
+function operateDisplay () {
+    switch (statusDisplay()) {
+        case 'empty':
+        case 'result':
+        case 'incomplete':
+        case 'message':
+            break
+        case 'begin':
+            leftNumber = rightNumber
+            rightNumber = ''
+            updateDisplay()
+            break
+        case 'complete':
+            const result = operate (onScreenOperator,leftNumber,rightNumber)
+            if (result === "ERRORDIVIDE0") {
+                rightNumber = 'do not divide by 0!'
+            }
+            else {
+                leftNumber = result
+                onScreenOperator = ''
+                rightNumber = ''
+            } 
+            updateDisplay()
+            break
+    }
+       
+}
+
+function pressOperator (operator) {
+
+    if (leftNumber === NaN) return
+
+    else if(rightNumber === '') {
+        onScreenOperator = operator
+        updateDisplay()
+    }
+
+    else  {
+        operateDisplay()
+    }
+}
+
+function pressDigit(digit) {
+
+}
